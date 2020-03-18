88ff75771189d4666421fd4e0c530f3fab2b0068
John Wells <molekewl2017@gmail.com>
2020-03-17 21:06:51 -0700

    set up knex & db

---
+++ migrations/20200317205204_cars.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.integer('VIN', 128).unique().notNullable();
+    tbl.string('Make').notNullable();
+    tbl.string('Model').notNullable();
+    tbl.integer('Mileage').notNullable();
+    tbl.string('Transmission Type');
+    tbl.string('Title');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
