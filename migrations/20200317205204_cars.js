f17a181c73cced886964cf70d53cc90276d9a543
John Wells <molekewl2017@gmail.com>
2020-03-18 08:57:11 -0700

    new table sales

--- migrations/20200317205204_cars.js
+++ migrations/20200317205204_cars.js
@@ -1,16 +1,25 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('cars', tbl => {
-    tbl.increments();
-    tbl.string('VIN', 128).unique().notNullable();
-    tbl.string('Make').notNullable();
-    tbl.string('Model').notNullable();
-    tbl.integer('Mileage').notNullable();
-    tbl.string('Transmission Type');
-    tbl.string('Title');
+  return knex.schema
+  .createTable('cars', tbl => {
+    tbl.increments()
+    tbl.string('VIN', 128).unique().notNullable()
+    tbl.string('Make').notNullable()
+    tbl.string('Model').notNullable()
+    tbl.integer('Mileage').notNullable()
+    tbl.string('Transmission Type')
+    tbl.string('Title')
   })
+  .createTable('sales', tbl => {
+    tbl.increments()
+    tbl.string('Sold')
+    tbl.string('Profit')
+    tbl.integer('car_id').references('id').inTable('cars')
+  });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('cars');
+  return knex.schema
+  .dropTableIfExists('cars')
+  .dropTableIfExists('sales')
 };
