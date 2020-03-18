82dc779fed1459ce018cad162fa8d0787cdd8bde
John Wells <molekewl2017@gmail.com>
2020-03-17 21:59:05 -0700

    get | post | put | delete

--- dealer/dealer-router.js
+++ dealer/dealer-router.js
@@ -19,4 +19,28 @@ router.post('/', (req, res) => {
     .catch(err => res.status(500).json({ error: 'Failed to add vehicle information' }));
 });
 
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+
+  knex('cars')
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(update => {
+      update > 0
+      ? res.status(200).json({ message: 'record updated successfully' })
+      : res.status(404).json({ message: 'The car with the specified Id does not exist' })
+    })
+    .catch(err => res.status(500).json({ error: 'Couldnt PUT to /api/cars/:id' }));
+});
+
+router.delete('/:id', (req, res) => {
+  knex('cars')
+    .where({ id: req.params.id })
+    .del()
+    .then(del => {
+      res.status(200).json(del)
+    })
+    .catch(err => res.status(500).json({ error: 'Failed to delete the vehicle' }))
+});
+
 module.exports = router;
